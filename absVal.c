130df3d7138ea3951f340631ec99579c4d83692e
JaeUk Park <jauek4010@gmail.com>
2025-04-08 16:01:23 +0900

    확정

---
+++ absVal.c
@@ -0,0 +1,9 @@
+/*
+ * absVal(x): return the absolute value of x.
+ * - Assume that INT_MIN (-2^31) is not provided as an input.
+ * - Ex) absVal(2) = 2, absVal(-1) = 1
+ */
+int absVal(int x) {
+	int isneg = (x >> 31);
+	return (x ^ isneg) + (isneg & 1);	
+}
